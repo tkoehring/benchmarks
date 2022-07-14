60fc09bd438fbd77e2aefb62a78614108720f19b
Travis Koehring <tkoehring@anl.gov>
2022-07-14 12:56:15 -0500

    pack_unpack arguments

--- src/yaksa/pack_unpack.c
+++ src/yaksa/pack_unpack.c
@@ -4,12 +4,10 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
-
+#include <math.h>
 #include <cuda_runtime_api.h>
-
 #include "yaksa.h"
 
-#define RUNS 100
 #define NUM_SIZES 27
 
 void parse_arguments(int, char**);
@@ -17,33 +15,48 @@ double pack_unpack(int);
 void *create_buf(int);
 void free_buf(void*);
 
-//Set by -g flag, use a gpu buffer
+//Set by -g flag. Use a gpu buffer
 int _gpu = 0;
+
+//Set by -r <val> flag. Indicates how many runs to perform
+int _runs = 0;
+
+//Set by the -M <val> flag. Indicates how big the buffers should be. Argument
+//asks for size in megabytes, then converted to bytes
+int _size = 0;
 
 int main(int argc, char **argv)
 {
-    const int buf_counts[] = {0, 1, 2, 4, 8, 16, 32, 64, 128, 256,
-                              512, 1024, 2048, 4096, 8192, 16384,
-                              32768, 65536, 131072, 262144, 524288,
-                              1048576, 2097152, 4194304, 8388608,
-                              16777216, 33554432};
+    parse_arguments(argc, argv);
 
-    double yaksa_times[NUM_SIZES];
+    int num_sizes = (log(_size) / log(2) + 2);
+    int *buf_counts = (int*)malloc(sizeof(int) * num_sizes);
+    double *yaksa_times = (double*)malloc(sizeof(double) * num_sizes);
     double yaksa_avg;
+
+    buf_counts[0] = 0;
+    buf_counts[1] = 1;
+
+    for(int i = 2; i < num_sizes; i++){
+        buf_counts[i] = (2 << (i - 2));
+    }
+
 
     yaksa_init(NULL);
     srand(time(NULL));
 
-    for(int i = 0; i < NUM_SIZES; i++)
+    for(int i = 0; i < num_sizes; i++)
     {
         yaksa_times[i] = pack_unpack(buf_counts[i]);
     }
 
-    for(int i = 0; i < NUM_SIZES; i++)
+    for(int i = 0; i < num_sizes; i++)
     {
         printf("%d,%.10f\n", buf_counts[i], yaksa_times[i]);
     }
 
+    free(buf_counts);
+    free(yaksa_times);
     yaksa_finalize();
 
     return 0;
@@ -51,11 +64,29 @@ int main(int argc, char **argv)
 
 void parse_arguments(int argc, char **argv){
     int opt;
-    while((opt = getopt (argc, argv, "g")) != -1){
+    while((opt = getopt (argc, argv, "gM:r:")) != -1){
         switch(opt){
             case 'g':
-                _gpu = 1;             
+                _gpu = 1;
+                break;
+            case 'M':
+                // Multiply the number of bytes in 1 MB
+                _size = atoi(optarg) * 1048576;
+                break;
+            case'r':
+                _runs = atoi(optarg);
+                break;
         }
+    }
+
+    //Set default value to 1MB
+    if(_size <= 0){
+        _size = 1048576;
+    }
+
+    //Default case for _runs
+    if(_runs <= 0){
+       _runs = 100;
     }
 }
 
@@ -76,13 +107,7 @@ double pack_unpack(int buf_count)
 
     double total_time = 0.0;
 
-    /* Populate input buffer */
-    for(int i=0; i < buf_count; i++)
-    {
-        input[i] = rand();    
-    }
-    
-    for(int i = 0; i < RUNS; i++)
+    for(int i = 0; i < _runs; i++)
     {
         //Start timer
         clock_t begin = clock();
@@ -96,7 +121,6 @@ double pack_unpack(int buf_count)
         //Wait for packing to complete
         rc = yaksa_request_wait(request);
         assert(rc == YAKSA_SUCCESS);
-
         //Start unpacking
         rc = yaksa_iunpack(pack_buf, buf_size, unpack_buf, buf_count,
                            YAKSA_TYPE__INT, 0, &actual_unpack_bytes,
@@ -106,7 +130,6 @@ double pack_unpack(int buf_count)
         //Wait for unpacking to complete
         rc = yaksa_request_wait(request);
         assert(rc == YAKSA_SUCCESS);
-
         //Stop timer
         clock_t end = clock();
         total_time += (double)(end - begin) / CLOCKS_PER_SEC;
@@ -116,11 +139,10 @@ double pack_unpack(int buf_count)
     free_buf(pack_buf);
     free_buf(unpack_buf);
 
-    return total_time / RUNS;
+    return total_time / _runs;
 }
 
 void *create_buf(int buf_size){
-
     if(_gpu){
         void *buf;
         (void*)cudaMalloc((void**)&buf, buf_size);
@@ -131,10 +153,11 @@ void *create_buf(int buf_size){
 }
 
 void free_buf(void *buf){
-    
     if(_gpu){
         cudaFree(buf);
+        return;
     } else {
         free(buf);
+        return;
     }
 }
